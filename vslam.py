5fd22d384a8c7f072f8d27e00da272714ccfd6fd
vik748 <shah.vi@husky.neu.edu>
2019-02-27 11:04:48 -0500

    speed tweaks

--- vslam.py
+++ vslam.py
@@ -19,7 +19,7 @@ np.set_printoptions(precision=3,suppress=True)
 
 parser = argparse.ArgumentParser(description='This is the simple VSLAM pipeline')
 parser.add_argument('-c', '--config', help='location of config file in yaml format',
-                    default='config/go_pro_icebergs_config.yaml') #go_pro_icebergs_config.yaml
+                    default='config/kitti_config.yaml') #go_pro_icebergs_config.yaml
 args = parser.parse_args()
  
 # Inputs, images and camera info
@@ -120,8 +120,8 @@ kp1_match_12  = np.expand_dims(np.array([o.pt for o in kp1],dtype='float32'),1)
 kp2_match_12, mask_klt, err = cv2.calcOpticalFlowPyrLK(gr1, gr2, kp1_match_12, None, **config_dict['KLT_settings'])
 print ("KLT tracked: ",np.sum(mask_klt) ," of total ",len(kp1_match_12),"keypoints")
 
-kp1_match_12 = kp1_match_12[mask_klt[:,0]==1]
-kp2_match_12 = kp2_match_12[mask_klt[:,0]==1]
+kp1_match_12 = kp1_match_12[mask_klt[:,0].astype(bool)]
+kp2_match_12 = kp2_match_12[mask_klt[:,0].astype(bool)]
 
 kp1_match_12_ud = cv2.undistortPoints(kp1_match_12,K,D)
 kp2_match_12_ud = cv2.undistortPoints(kp2_match_12,K,D)
@@ -151,14 +151,16 @@ plt.pause(0.001)
 input("Press [enter] to continue.")
 
 # Trim the tracked key pts
-kp1_match_12 = kp1_match_12[mask_RP_12[:,0]==1]
-kp2_match_12 = kp2_match_12[mask_RP_12[:,0]==1]
+kp1_match_12 = kp1_match_12[mask_RP_12[:,0].astype(bool)]
+kp2_match_12 = kp2_match_12[mask_RP_12[:,0].astype(bool)]
 
-kp1_match_12_ud = kp1_match_12_ud[mask_RP_12[:,0]==1]
-kp2_match_12_ud = kp2_match_12_ud[mask_RP_12[:,0]==1]
+kp1_match_12_ud = kp1_match_12_ud[mask_RP_12[:,0].astype(bool)]
+kp2_match_12_ud = kp2_match_12_ud[mask_RP_12[:,0].astype(bool)]
 
 landmarks_12, mask_tri_12 = triangulate(np.eye(4), T_1_2, kp1_match_12_ud, 
                                        kp2_match_12_ud, None)
+
+
 
 fig2 = plt.figure(2)
 ax2 = fig2.add_subplot(111, projection='3d')
@@ -229,23 +231,31 @@ PROCESS FRAME
 frame_no = 3
 def process_frame(img_curr, mask_curr, gr_prev, kp_prev_matchpc, kp_prev_cand, lm_prev, T_prev, corners_prev_ud):
     global frame_no, cam_pose_trail, cam_trail_pts, cam_pose
+    global inliers
+    time_start = time.time()
     gr_curr = cv2.cvtColor(img_curr,cv2.COLOR_BGR2GRAY)
+    print("Time elapsed in converting to gray: ",time.time()-time_start)
+
     if USE_CLAHE: gr_curr = clahe.apply(gr_curr)
+    print("Time elapsed in CLAHE: ",time.time()-time_start)
+
     
     kp_curr_matchpc, mask_klt, _ = cv2.calcOpticalFlowPyrLK(gr_prev, gr_curr, 
                                     kp_prev_matchpc, None, **config_dict['KLT_settings'])
     kp_curr_cand_matchpc, mask_cand_klt, _ = cv2.calcOpticalFlowPyrLK(gr_prev, gr_curr, 
                                               kp_prev_cand, None, **config_dict['KLT_settings'])
+    
+    print("Time elapsed in optical flow: ",time.time()-time_start)
 
     print ("KLT Tracked: ",np.sum(mask_klt)," of total ",len(kp_prev_matchpc),"keypoints")
     print ("KLT Candidates Tracked: ",np.sum(mask_cand_klt)," of total ",len(kp_prev_cand),"keypoints")
 
     # Filter out points that are not tracked
-    kp_prev_matchpc = kp_prev_matchpc[mask_klt[:,0]==1]
-    kp_curr_matchpc = kp_curr_matchpc[mask_klt[:,0]==1]
-    kp_prev_cand = kp_prev_cand[mask_cand_klt[:,0]==1]
-    kp_curr_cand_matchpc = kp_curr_cand_matchpc[mask_cand_klt[:,0]==1]
-    lm_prev = lm_prev[mask_klt[:,0]==1]
+    kp_prev_matchpc = kp_prev_matchpc[mask_klt[:,0].astype(bool)]
+    kp_curr_matchpc = kp_curr_matchpc[mask_klt[:,0].astype(bool)]
+    kp_prev_cand = kp_prev_cand[mask_cand_klt[:,0].astype(bool)]
+    kp_curr_cand_matchpc = kp_curr_cand_matchpc[mask_cand_klt[:,0].astype(bool)]
+    lm_prev = lm_prev[mask_klt[:,0].astype(bool)]
     
     # Merge tracked LM and candidate pts to filter together in findEssential
     kp_prev_all = np.vstack((kp_prev_matchpc, kp_prev_cand))
@@ -254,9 +264,13 @@ def process_frame(img_curr, mask_curr, gr_prev, kp_prev_matchpc, kp_prev_cand, l
     kp_prev_all_ud = cv2.undistortPoints(kp_prev_all,K,D)
     kp_curr_all_ud = cv2.undistortPoints(kp_curr_all,K,D)
 
+    print("Time elapsed in undistort: ",time.time()-time_start)
+
     E, mask_e_all = cv2.findEssentialMat(kp_prev_all_ud, kp_curr_all_ud, focal=1.0, pp=(0., 0.), 
                                    method=cv2.RANSAC, prob=0.999, threshold=0.001)
-    essen_mat_pts = np.sum(mask_e_all)    
+    essen_mat_pts = np.sum(mask_e_all)  
+    print("Time elapsed in find E: ",time.time()-time_start)
+
     
     print ("Essential matrix: used ",essen_mat_pts ," of total ",len(kp_curr_all),"matches")
     '''
@@ -275,23 +289,25 @@ def process_frame(img_curr, mask_curr, gr_prev, kp_prev_matchpc, kp_prev_cand, l
         gr_curr_masked = cv2.addWeighted(mask_curr, 0.2, gr_curr, 1 - 0.2, 0)
     else: gr_curr_masked = gr_curr
     
-    print(mask_RP_cand)
-
     img_track_feat = draw_point_tracks(kp_prev_matchpc,gr_curr_masked,
                                        kp_curr_matchpc,mask_RP_feat, False)
     img_track_all = draw_point_tracks(kp_prev_cand,img_track_feat,
                                       kp_curr_cand_matchpc,mask_RP_cand, False, 
                                       color=[255,255,0])
+    print("Time elapsed in drawing tracks: ",time.time()-time_start)
 
-    fig1_image.set_data(img_track_all)
-    fig1.canvas.draw_idle(); plt.pause(0.01)
-    if PAUSES: input("Press [enter] to continue.")
 
-    lm_updated = lm_prev[mask_RP_feat[:,0]==1]
+    #fig1_image.set_data(img_track_all)
+    #fig1.canvas.draw_idle(); plt.pause(0.01)
+    #if PAUSES: input("Press [enter] to continue.")
+
+    lm_updated = lm_prev[mask_RP_feat[:,0].astype(bool)]
     print("LM prev was: ",len(lm_prev), " LM updated after Ess mat filter is :",len(lm_updated))
-    kp_curr_matchpc = kp_curr_matchpc[mask_RP_feat[:,0]==1]
+    kp_curr_matchpc = kp_curr_matchpc[mask_RP_feat[:,0].astype(bool)]
            
     success, T_cur, inliers = T_from_PNP(lm_updated, kp_curr_matchpc, K, D)
+    print("Time elapsed in PNP: ",time.time()-time_start)
+
     print("PNP inliers: ",len(inliers), " of ",len(lm_updated))
     if not success:
         print ("PNP faile in frame ",frame_no," Exiting...")
@@ -310,44 +326,47 @@ def process_frame(img_curr, mask_curr, gr_prev, kp_prev_matchpc, kp_prev_cand, l
     kp_prev_cand_ud = kp_prev_all_ud[-len(kp_prev_cand):]
     kp_curr_cand_ud = kp_curr_all_ud[-len(kp_prev_cand):]
     
-    #print("kp_prev_cand_ud: ", kp_prev_cand_ud.shape, " kp_curr_cand_ud: ",kp_curr_cand_ud.shape)
-
-    kp_prev_cand = kp_prev_cand[mask_RP_cand[:,0]==1]    
-    kp_curr_cand_matchpc = kp_curr_cand_matchpc[mask_RP_cand[:,0]==1]    
-    kp_prev_cand_ud = kp_prev_cand_ud[mask_RP_cand[:,0]==1]
-    kp_curr_cand_ud = kp_curr_cand_ud[mask_RP_cand[:,0]==1]
+    kp_prev_cand = kp_prev_cand[mask_RP_cand[:,0].astype(bool)]    
+    kp_curr_cand_matchpc = kp_curr_cand_matchpc[mask_RP_cand[:,0].astype(bool)]    
+    kp_prev_cand_ud = kp_prev_cand_ud[mask_RP_cand[:,0].astype(bool)]
+    kp_curr_cand_ud = kp_curr_cand_ud[mask_RP_cand[:,0].astype(bool)]
     
     lm_cand, mask_tri = triangulate(T_prev, T_cur, kp_prev_cand_ud, 
                                     kp_curr_cand_ud, None)
-    print("Shape of mask_tri: ", mask_tri.shape)
-    #print(kp_curr_cand.shape)
+    print("Time elapsed in triangulate: ",time.time()-time_start)
+
     print("Point after rejection in triangulation: ", np.sum(mask_tri)," out of length: ", len(mask_tri))
     
     img_rej_pts = draw_point_tracks(kp_prev_cand, img_track_all, 
                                     kp_curr_cand_matchpc, 1-mask_tri, False, color=[255,0,0])
-    fig1_image.set_data(img_rej_pts)
-    fig1.canvas.draw_idle(); plt.pause(0.1)
-    
-    #lm_updated = lm_prev[mask_RP_feat[:,0]==1]
-    kp_curr_cand_matchpc = kp_curr_cand_matchpc[mask_tri[:,0]==1]
+    print("Time elapsed in draw pt tracks: ",time.time()-time_start)
 
-    graph_pnp = plot_3d_points(ax2, lm_cand, linestyle="", color='r', marker=".", markersize=2)
+    #fig1_image.set_data(img_rej_pts)
+    #fig1.canvas.draw_idle(); #plt.pause(0.01)
+    print("Time elapsed in draw pt track SET DATA: ",time.time()-time_start)
+
+    #lm_updated = lm_prev[mask_RP_feat[:,0]==1]
+    kp_curr_cand_matchpc = kp_curr_cand_matchpc[mask_tri[:,0].astype(bool)]
+
+    new_lm_graph = plot_3d_points(ax2, lm_cand, linestyle="", color='r', marker=".", markersize=2)
     plot_pose3_on_axes(ax2, T_cur, axis_length=2.0, center_plot=True, line_obj_list=cam_pose)
     
     cam_trail_pts = np.append(cam_trail_pts,T_cur[:3,[-1]].T,axis=0)
     cam_pose_trail = plot_3d_points(ax2,cam_trail_pts , line_obj=cam_pose_trail, linestyle="", color='g', marker=".", markersize=2)
-    fig2.canvas.draw_idle(); plt.pause(0.05)
+    fig2.canvas.draw_idle(); #plt.pause(0.01)
     if PAUSES: input("Press [enter] to continue.")
 
     # Remove pose lines and landmarks to speed plotting
     if PLOT_LANDMARKS:
         graph_newlm = plot_3d_points(ax2, lm_cand, linestyle="", color='C0', marker=".", markersize=2)    
-        fig2.canvas.draw_idle(); plt.pause(0.01)
+        fig2.canvas.draw_idle(); #plt.pause(0.01)
     
     
     lm_updated = np.concatenate((lm_updated,lm_cand))
     kp_curr_matchpc = np.concatenate((kp_curr_matchpc,kp_curr_cand_matchpc))
     
+    print("Time elapsed in beofre orb: ",time.time()-time_start)
+
     kp_curr_cand = detector.detect(gr_curr,mask_curr)
     print ("New feature candidates detected: ",len(kp_curr_cand))
     
@@ -363,15 +382,16 @@ def process_frame(img_curr, mask_curr, gr_prev, kp_prev_matchpc, kp_prev_cand, l
          
     kp_curr_cand_pts = remove_redundant_newkps(kp_curr_cand_pts, kp_curr_matchpc, RADIAL_NON_MAX_RADIUS)
     
+    print("Time elapsed in orb, filtering, radial: ",time.time()-time_start)
+
     print ("Candidate points after redudancy check against current kps: ",len(kp_curr_cand_pts))
-    
-    img_cand_pts = draw_points(img_track_all,kp_curr_cand_pts[:,0,:], color=[255,255,0])
+    img_cand_pts = draw_points(img_rej_pts,kp_curr_cand_pts[:,0,:], color=[255,255,0])
     fig1_image.set_data(img_cand_pts)
     fig1.canvas.draw_idle(); plt.pause(0.05)
     
     frame_no += 1
     
-    graph_pnp.remove()
+    new_lm_graph.remove()
 
     print ("FRAME deq "+str(frame_no)+" COMPLETE")
 
@@ -387,6 +407,11 @@ if USE_MASKS:
 else:
     mask = None
 
+print(kp2_match_12.shape)
+kp2_match_12 = kp2_match_12[mask_tri_12[:,0].astype(bool)]
+print(kp2_match_12.shape)
+
+
 out = process_frame(img3, mask, gr2, kp2_match_12, kp2_pts, landmarks_12, T_1_2, corners2_ud)
     
 print ("\n \n FRAME 3 COMPLETE \n \n")
@@ -400,8 +425,10 @@ for i in range(init_imgs_indx[1]+img_step*2,len(images),img_step):
     if USE_MASKS:
         mask = cv2.imread(masks[i],cv2.IMREAD_GRAYSCALE)
     print("Processing image: ",images[i])
-    img = cv2.imread(images[i])
     st = time.time()
+    img = cv2.imread(images[i])
+    print("Time to read last image: ",time.time()-st)
+
     out = process_frame(img, mask, *out)
     print("Time to process last frame: ",time.time()-st)
     plt.pause(0.001)
