1bb0b7151d7827bab11d89f16a649ce122dd79c6
vik748 <shah.vi@husky.neu.edu>
2020-05-13 18:50:58 -0400

    Adding gtsam to CI
    debugging ci
    
    debugging ci
    
    debugging gtsam install script
    
    debug ci
    
    debug ci
    
    debug ci
    
    Adding timeout to ci
    
    Adding artifacts from build to test
    
    Update .gitlab-ci.yml
    
    fixing typo in ci
    
    Update .gitlab-ci.yml
    
    increasing timeout
    
    debugging ci
    
    debug ci
    
    more debugging

---
+++ install/build_and_install_gtsam.sh
@@ -0,0 +1,18 @@
+# Install fbow
+set -x
+CWD=`pwd`
+git clone --depth 1 https://github.com/borglab/gtsam gtsam
+
+cd gtsam
+
+pip install -r cython/requirements.txt
+
+mkdir build
+cd build
+
+cmake -DGTSAM_ALLOW_DEPRECATED_SINCE_V4=OFF \
+      -DGTSAM_INSTALL_MATLAB_TOOLBOX=OFF \
+      -DGTSAM_INSTALL_CYTHON_TOOLBOX=ON \
+      -DCMAKE_INSTALL_PREFIX=$CWD/gtsam_bin .. && make -j$(nproc) && make install
+
+cd ../..
