e3373fa6cf61682f80b49a9e6a958b207fb3049a
Arvind Chelvan <arvindtchelvan@gmail.com>
2021-06-16 22:16:11 -0400

    added documentation

--- helper_functions/GTSAM_helper.py
+++ helper_functions/GTSAM_helper.py
@@ -1,5 +1,5 @@
 """
-Wrappers for GTSAM cython for seamless implementation with python/numpy
+Wrappers for GTSAM for seamless implementation with python/numpy
 """
 from __future__ import print_function
 
@@ -19,6 +19,14 @@ from frame import Frame
 
 
 class iSAM2Wrapper():
+    """
+    GTSAM wrapper class that aids seamless integration with
+    python/numpy
+
+    Contains helper functions that act as a layer of abstraction
+    around GTSAM
+    """
+
     def __init__(self, pose0=np.eye(4), pose0_to_pose1_range=1.0, K=np.eye(3),
                  relinearizeThreshold=0.1, relinearizeSkip=10, proj_noise_val=1.0):
         self.graph = NonlinearFactorGraph()
@@ -53,6 +61,20 @@ class iSAM2Wrapper():
         self.lm_factor_ids = set()
 
     def add_GenericProjectionFactorCal3_S2_factor(self, pt_uv, X_id, L_id):
+        """
+        Adds a landmark point to factor graph
+
+        Function adds a set of Landmark points in succession. The
+        factor is generated using GTSAM function
+        GenericProjectionFactorCal3_S2. The factor is pushed back
+        into self.graph
+
+        :param pt_uv: ndarray, set of landmark points of shape Nx2
+        :param X_id: int, key number of pose from which landmarks are
+        observed
+        :param L_id: int64 ndarray, array of landmark key numbers
+        """
+
         if pt_uv.ndim == 1:
             raise ValueError("Supplied point is 1-dimensional, required Nx2 array")
         if pt_uv.shape[1] != 2:
@@ -67,10 +89,24 @@ class iSAM2Wrapper():
         self.lm_factor_ids.update(L_id)
 
     def add_PoseEstimate(self, X_id, T):
+        """
+        Adds a pose estimate to self.initial_estimate
+
+        :param X_id: int, key number of pose to be added
+        :param T: float64 ndarray, 4x4 transformation matrix
+        """
+
         self.initial_estimate.insert(iSAM2Wrapper.get_key('x', X_id),
                                      gtsam.gtsam.Pose3(T))
 
     def add_LandmarkEstimate(self, L_id, Pt_estimate):
+        """
+        Adds landmark estimates to self.initial_estimate
+
+        :param L_id: int64 ndarray, array of landmark key numbers
+        :param Pt_estimate: float64 ndarray, set of landmark points
+        """
+
         if Pt_estimate.ndim == 1:
             raise ValueError("Supplied point is 1-dimensional, required Nx3 array")
         if Pt_estimate.shape[1] != 3:
@@ -80,6 +116,14 @@ class iSAM2Wrapper():
                                          p_est)
 
     def update(self, iterations=1):
+        """
+        Performs ISAM2 update steps
+
+        Number of update steps given by parameter iterations.
+
+        :param iterations: int, default=1. number of update steps
+        """
+
         self.isam2.update(self.graph, self.initial_estimate)
         # Perform additional iterations as specified
         for i in range(2, iterations + 1):
@@ -90,15 +134,35 @@ class iSAM2Wrapper():
         self.initial_estimate.clear()
 
     def get_Estimate(self):
+        """
+        Returns current estimate
+
+        :return: self.current estimate
+        """
+
         return self.current_estimate
 
     def get_landmark_estimates(self):
+        """
+        Function to get landmarks and their ids
+
+        :return: [landmarks, landmark ids]
+        """
+
         lm = []
         for l_id in self.lm_factor_ids:
             lm += [self.current_estimate.atPoint3(iSAM2Wrapper.get_key('l', l_id))]
         return np.array(lm), list(self.lm_factor_ids)
 
     def get_curr_Pose_Estimate(self, x_id):
+        """
+        Function to get current pose estimate
+
+        :param x_id: int, key number of pose to be returned
+
+        :return: current estimate of given pose id
+        """
+
         return self.current_estimate.atPose3(iSAM2Wrapper.get_key('x', x_id)).matrix()
 
     def plot_estimate(self, fignum=0):
@@ -107,6 +171,7 @@ class iSAM2Wrapper():
         Author: Ellon Paiva
         Based on MATLAB version by: Duy Nguyen Ta and Frank Dellaert
         """
+
         fig = plt.figure(fignum)
         axes = fig.gca(projection='3d')
         plt.cla()
@@ -134,21 +199,40 @@ class iSAM2Wrapper():
 
     @staticmethod
     def pt_dist(pt1, pt2):
+        """
+        Returns the euclidean distance between two 3D points
+
+        :param pt1: First point
+        :param pt2: Second point
+
+        :return: Euclidean distance between pt1 and pt2
+        """
+
         return ((pt1.x() - pt2.x()) * (pt1.x() - pt2.x()) + (pt1.y() - pt2.y()) * (pt1.y() - pt2.y()) + (
                     pt1.z() - pt2.z()) * (pt1.z() - pt2.z())) ** .5
 
     @staticmethod
     def symbol(name: str, index: int) -> int:
         """ helper for creating a symbol without explicitly casting 'name' from str to int """
+
         return gtsam.symbol(ord(name), index)
 
     @staticmethod
     def key_label(key_id):
+        """
+        Separates symbol character and symbol index
+
+        :param key_id: symbol to be separated
+
+        :return: symbol character, symbol index
+        """
+
         return str(chr(symbolChr(key_id))), symbolIndex(key_id)
 
     @staticmethod
     def plot_line_on_axes(axes, point1, point2, linespec):
         """Plot a 3D point on given axis 'axes' with given 'linespec'."""
+
         xs = np.array([point1.x(), point2.x()])
         ys = np.array([point1.y(), point2.y()])
         zs = np.array([point1.z(), point2.z()])
@@ -158,12 +242,17 @@ class iSAM2Wrapper():
     @staticmethod
     def plot_line(fignum, point1, point2, linespec):
         """Plot a 3D point on given figure with given 'linespec'."""
+
         fig = plt.figure(fignum)
         axes = fig.gca(projection='3d')
         plot_line_on_axes(axes, point1, point2, linespec)
 
     @staticmethod
     def draw_graph(graph, values, fig_num=1):
+        """
+        plots the poses given in values
+        """
+
         for i in range(graph.size()):
             factor = graph.at(i)
             fkeys = factor.keys()
@@ -180,15 +269,26 @@ class iSAM2Wrapper():
 
     @staticmethod
     def get_key(letter, number):
-        """Create key for pose id number."""
+        """
+        Create key for pose id number
+
+        :param letter: letter of key to be retrieved
+        :param number: number of key to be retrieved
+        :return: key
+        """
+
         return int(gtsam.symbol(letter, number))
 
     @staticmethod
     def CameraMatrix_to_Cal3_S2(K):
-        '''
-        Convert 3x3 camera matrix to 5 length vector for Cal3_S2 given as 
+        """
+        Convert 3x3 camera matrix to 5 length vector for Cal3_S2 given as
         Cal3_S2 (double fx, double fy, double s, double u0, double v0)
-        '''
+
+        :param K: 3x3 camera matrix
+        :return: camera matrix in Cal3_S2 format
+        """
+
         if not np.allclose(K, np.triu(K)):
             raise ValueError('K matrix not upper triangular, might be incorrrect')
         fx, fy, s, u0, v0 = K[0, 0], K[1, 1], K[0, 1], K[0, 2], K[1, 2]
@@ -227,6 +327,13 @@ class iSAM2Wrapper():
     '''
 
     def add_keyframe_factors(self, fr_j):
+        """
+        Adds a landmark to factor graph and initial estimate if it's
+        observed in at least three frames
+
+        :param fr_j: current operational frame
+        """
+
         new_lm_fact = 0
         exist_lm_fact = 0
         for l, l_id in zip(Frame.landmark_array[fr_j.lm_ind], fr_j.lm_ind):
